35afb112b612e284fa4b8d518c0531b87c581d30
Bruno Leandro <bruno.lbs100@gmail.com>
2022-06-18 20:13:10 -0300

    Configuração dos campos que serão exibidos

--- app.rb
+++ app.rb
@@ -3,4 +3,20 @@ require "sinatra/contrib"
 require "sinatra/reloader"
 require "sinatra/namespace"
 
+Mongoid.load! "mongoid.config"
+
+class Tweet 
+    include Mongoid::Document 
+    include Mongoid::Timestamps
+
+    field :author, type: string, default: "Anon"
+    field :content, type: string
+    field :image, type: string
+    field :tags, type: string, default: []
+    field :likes, type: integer, default: 0
+    field :author, type: string
+end
+
+
+
 get ("/api/v1/tweets") { "Hello World"}
