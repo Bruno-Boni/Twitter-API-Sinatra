8eb1ac414df8b47e53f31c8750d58e98bb01c063
Bruno Leandro <bruno.lbs100@gmail.com>
2022-06-18 20:28:30 -0300

    Colocando a gema mongoid no arquivo principal

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require "sinatra"
 require "sinatra/contrib"
 require "sinatra/reloader"
 require "sinatra/namespace"
+require "mongoid"
 
 Mongoid.load! "mongoid.config"
 
@@ -9,12 +10,12 @@ class Tweet
     include Mongoid::Document 
     include Mongoid::Timestamps
 
-    field :author, type: string, default: "Anon"
-    field :content, type: string
-    field :image, type: string
-    field :tags, type: string, default: []
-    field :likes, type: integer, default: 0
-    field :author, type: string
+    field :author, type: String, default: "Anon"
+    field :content, type: String
+    field :image, type: String
+    field :tags, type: Array, default: []
+    field :likes, type: Integer, default: 0
+    field :author, type: String
 end
 
 
