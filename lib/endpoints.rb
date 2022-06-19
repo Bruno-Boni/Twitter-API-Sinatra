78fa2fa999d1b682813e5e4911766d309183dcb6
Bruno Leandro <bruno.lbs100@gmail.com>
2022-06-19 00:37:47 -0300

    Criação de um novo endpoint para acessar os tweets através dos seus ids

--- lib/endpoints.rb
+++ lib/endpoints.rb
@@ -5,6 +5,9 @@ namespace "/api/v1" do
     get "/tweets" do
         Tweet.all.to_json
        end
+    get "/tweets/:id" do |id| 
+        Tweet.find(id).to_json
+    end
     post "/tweets" do
         new_tweet = Tweet.new(JSON.parse(request.body.read)).save
     end
